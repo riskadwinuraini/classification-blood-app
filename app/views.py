957585e163a3f38ae3cb36bfae29784b41dfde3a
riskadwinuraini <74886545+riskadwinuraini@users.noreply.github.com>
2024-03-09 13:22:42 +0700

    perbaiki code

--- app/views.py
+++ app/views.py
@@ -1,10 +1,10 @@
-from flask import render_template
 from app import app
+
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return "Hello, World!"
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+# @app.route('/about')
+# def about():
+#     return render_template('about.html')
