ca28119c1f8fda038c19aeb418b80592a0c21303
riskadwinuraini <74886545+riskadwinuraini@users.noreply.github.com>
2024-03-13 12:35:17 +0700

    update

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
 from app import app
-
+from flask import render_template
 
 @app.route('/')
 def home():
     return "Hello, World!"
 
-# @app.route('/about')
-# def about():
-#     return render_template('about.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
